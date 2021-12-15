15d56f91dc154c3c0eda34a90944272654c6eda3
ArnaVala <arnavala@gmail.com>
2021-12-15 09:18:24 +0000

    Spotify login auth working!

--- pages/login.js
+++ pages/login.js
@@ -2,12 +2,17 @@ import {getProviders, signIn} from "next-auth/react";
 
 function Login({providers}) {
   return (
-    <div>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img className="w-48 mb-5" src="https://links.papareact.com/9xl" alt="login into spotify" />
       
       { Object.values(providers).map((provider)=> (
        <div key={provider.name}>
-          <button>Login with {provider.name}</button>
+          <button //after logging in - where do you go?
+            className="text-white p-5 bg-[#18D860] rounded-full mt-4"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
         </div>
       ))}
     </div>
