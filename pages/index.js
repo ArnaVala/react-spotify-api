3cf34ee5e8e9a7adb238b33d06d55e13a15b400c
ArnaVala <arnavala@gmail.com>
2021-12-15 06:10:12 +0000

    project setup and Sidebar component build

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1>Spotify App</h1>
-
+    <div className="">
+      <main>
+        <Sidebar />
+        {/**Center content*/}
+      </main>
+      <div>{/**Player control*/}</div>
     </div>
   )
 }
